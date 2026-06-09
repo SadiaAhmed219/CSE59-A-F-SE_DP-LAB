ec5926309d32dd33663b3b2fe3b69e0efea60372
SadiaAhmed219 <sadia58469@gmail.com>
2026-06-09 10:59:40 +0600

    Update YouDo4.java

--- Task 2/YouDo4.java
+++ Task 2/YouDo4.java
@@ -1,1 +1,17 @@
-
+class Sensor {
+    String name;
+    int batteryLevel;
+    
+    void charge() {
+        batteryLevel = 100;
+        System.out.println("Sensor fully charged");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Sensor s = new Sensor();
+        
+        s.charge();
+        System.out.println("Battery Level: " + s.batteryLevel);
+    }
+}
