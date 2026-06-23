0f2d0bd82ce63ade0d0016828f9cc636f3b39629
SadiaAhmed219 <sadia58469@gmail.com>
2026-06-23 11:30:43 +0600

    Update Main4.java

--- Task 4/Main4.java
+++ Task 4/Main4.java
@@ -1,1 +1,37 @@
+// Abstraction
+interface MessageSender {
+    void sendMessage(String message);
+}
 
+// Low-level implementation
+class EmailSender implements MessageSender {
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+// High-level module
+class NotificationService {
+
+    private MessageSender messageSender;
+
+    // Dependency Injection
+    public NotificationService(MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }
+
+    public void alertUser(String msg) {
+        messageSender.sendMessage(msg);
+    }
+}
+
+// Main class
+public class Main {
+    public static void main(String[] args) {
+
+        MessageSender sender = new EmailSender();
+        NotificationService service = new NotificationService(sender);
+
+        service.alertUser("Hello! DIP Working Example");
+    }
+}
