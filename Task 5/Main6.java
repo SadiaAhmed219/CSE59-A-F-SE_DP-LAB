398f58c84b641530f73343edf5da55d7a65a862c
SadiaAhmed219 <sadia58469@gmail.com>
2026-06-30 10:57:10 +0600

    Update Main6.java

--- Task 5/Main6.java
+++ Task 5/Main6.java
@@ -1,1 +1,25 @@
+interface HttpService {
+    void handleHttpRequest();
+}
 
+interface DatabaseManager {
+    void executeSqlStatement();
+}
+interface ContainerOps {
+    void restartDockerContainer();
+
+}
+
+class WebController implements HttpService {
+    public void handleHttpRequest(){
+        System.out.println("Handing HTTP Request");
+    }
+}
+public class Main6 {
+
+    public static void main(String[] args) {
+
+        WebController controller = new WebController();
+        controller.handleHttpRequest();
+    }
+}
