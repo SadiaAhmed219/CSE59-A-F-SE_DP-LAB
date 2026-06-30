be4f0f146408748eed578303e30005f49c3d3f4b
SadiaAhmed219 <sadia58469@gmail.com>
2026-06-30 10:57:33 +0600

    Update Main7.java

--- Task 5/Main7.java
+++ Task 5/Main7.java
@@ -1,1 +1,31 @@
+interface InetworkProtocol {
+    void broadcastMessage(Strinhg msg);
+}
 
+//Implementtation
+class BluetoothMeshNetwork implements INetworkProtocol {
+    public void broadcastMessage(String msg) {
+        System.out.println("Broadcast:" + msg);
+    }
+}
+// High-Level class
+class DisasterAlertSystem {
+    private INetworkprotocol network;
+    public DisasteralertSystem(INetworkProtocol network) {
+        this.network = network;
+    }
+    public void triggerAlert(String msg) {
+        network.broadcastMessage(msg);
+    }
+
+}
+public class Main7 {
+
+    public static void main(String[] args) {
+
+        DisasterAlertSystem alert =
+                new DisasterAlertSystem(new BluetoothMeshNetwork());
+
+        alert.triggerAlert("Flood Warning!");
+    }
+}
