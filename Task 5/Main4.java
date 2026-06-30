150048d841f1ce9cd428c8f1dedf42740ff01660
SadiaAhmed219 <sadia58469@gmail.com>
2026-06-30 10:56:12 +0600

    Update Main4.java

--- Task 5/Main4.java
+++ Task 5/Main4.java
@@ -1,1 +1,30 @@
+interface OptimizableModel {
+    void quantize();
+}
+class ResNet18 implements OptimizableModel {
+    public void quantize() {
+        System.out.println("Applying Int8 Quantization for Resnet18");
+    }
+}
+class MobileNet implements OptimizableModel {
 
+    public void quantize() {
+        System.out.println("Applying Dynamic Quantization for MobileNet");
+    }
+
+}
+class InterfaceEngine {
+    public void optimize(OptimizableModel Model) {
+        model.quantize();
+    }
+}
+public class Main4 {
+
+    public static void main(String[] args) {
+
+        InferenceEngine engine = new InferenceEngine();
+
+        engine.optimize(new ResNet18());
+        engine.optimize(new MobileNet());
+    }
+}
