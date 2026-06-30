662c7dff9b28163fa8b16c2627728a06fd9fad01
SadiaAhmed219 <sadia58469@gmail.com>
2026-06-30 10:56:42 +0600

    Update Main5.java

--- Task 5/Main5.java
+++ Task 5/Main5.java
@@ -1,1 +1,35 @@
+class UniversityMember {
+    public void attendLecture() {
+        System.out.println("Attending Lecture");
+    }
+}
+interface Researcher {
+    void conductLabResearch();
+}
+class UndergraduateStudent extends UniversityMember {
+}
+class GraduateResearchAssistant extends UniversityMember implements Researcher {
+    public void conductLabResearch(){
+        System.out.println("Conducting Lab Research");
+    }
+}
+class  Professor extends UniversityMember implements Reserach {
+    public void conductLabResearch(){
+
+System.out.println("Professor Research");
+    }
+}
+public class Main5 {
+
+    public static void main(String[] args) {
+
+        UndergraduateStudent student = new UndergraduateStudent();
+        student.attendLecture();
+
+        Professor professor = new Professor();
+        professor.attendLecture();
+        professor.conductLabResearch();
+    }
+}
+
 
