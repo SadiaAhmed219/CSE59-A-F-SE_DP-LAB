0c2ea49c369cf7f59c50b0e6806a4b9e9b02fc84
SadiaAhmed219 <sadia58469@gmail.com>
2026-06-30 10:55:20 +0600

    Update Main2.java

--- Task 5/Main2.java
+++ Task 5/Main2.java
@@ -1,1 +1,32 @@
+interface MessageSender {
+    void sendMessage(String message);
+}
 
+class EmailSender implements MessageSender {
+    
+    public void sendMessage(String message) {
+        System.out.println("Email Sent: " + message);
+    }
+}
+
+class NotificationService {
+
+    private MessageSender sender;
+
+    public NotificationService(MessageSender sender){
+        this.sender = sender;
+    }
+     public void alertUser(String msg) {
+        sender.sendMessage(msg);
+    }
+}
+public class Main2 {
+
+    public static void main(String[] args) {
+
+        NotificationService service =
+                new NotificationService(new EmailSender());
+
+        service.alertUser("Welcome!");
+    }
+}
